5acf14359bc5c06d58258a8cbc1a11d518042fbb
Soumyadev Desktop <soumyadev.poriya@gmail.com>
2023-01-14 21:27:28 +0530

    syntax error in python code is resolved

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -3,22 +3,15 @@ from time import sleep
 from typing import Optional
 
 from bs4 import BeautifulSoup
-from kafka import KafkaConsumer, KafkaProducer
+from kafka import KafkaConsumer
 from json import loads
 
-consumer = KafkaConsumer(
-    'my-topic',
-     bootstrap_servers=['my-cluster-kafka-bootstrap:9092'],
-     auto_offset_reset='earliest'
-     #enable_auto_commit=True,
-     #group_id='default'
-     #value_deserializer=lambda x: loads(x.decode('utf-8')))
-     )
+consumer = KafkaConsumer('my-topic', bootstrap_servers=['my-cluster-kafka-bootstrap:9092'], auto_offset_reset='earliest')
 
-i=0
+i = 0
 for message in consumer:
     message = message.value
     print(message)
-    i=i+1
+    i = i+1
 
 print(" "+i)
