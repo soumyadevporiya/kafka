634d058ceed86cbdb6fa9edea2c7cea53e3e257e
Soumyadev Desktop <soumyadev.poriya@gmail.com>
2023-01-14 22:54:49 +0530

    few files changed for writing message to GCP bucket

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,11 +1,19 @@
 from kafka import KafkaConsumer
-
+from google.cloud import storage
 consumer = KafkaConsumer('my-topic', bootstrap_servers=['my-cluster-kafka-bootstrap:9092'], auto_offset_reset='earliest' )
 
+client = storage.Client()
+
+bucket = client.bucket("soumyabucket")
+
+
+
+
 i = 0
+
 for message in consumer:
     message = message.value
-    print(message)
+    bucket.blob('first_dist00000.txt').upload_from_string(message, 'text/csv')
     i = i+1
 
-print(" "+i)
+#print(" "+i)
