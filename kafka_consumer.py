1bf9bbedb7320171311761de6799837eaf1fe0b5
Soumyadev Desktop <soumyadev.poriya@gmail.com>
2023-01-16 14:31:10 +0530

    consumer slightly changed

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,8 +1,9 @@
 from kafka import KafkaConsumer
 from google.cloud import storage
-consumer = KafkaConsumer('my-topic', bootstrap_servers=['my-cluster-kafka-bootstrap:9092'], auto_offset_reset='earliest' )
+#consumer = KafkaConsumer('my-topic', bootstrap_servers=['my-cluster-kafka-bootstrap:9092'], auto_offset_reset='earliest' )
 
 #consumer = KafkaConsumer('admintome-test', bootstrap_servers=['kafka-service:9092'], auto_offset_reset='earliest' )
+consumer = KafkaConsumer('my-topic', bootstrap_servers=['my-cluster-kafka-bootstrap:9092'], auto_offset_reset='latest' )
 
 client = storage.Client()
 
@@ -16,9 +17,10 @@ for message in consumer:
     s = bucket.blob('first_dist00000.txt').download_as_string()
     #s1 = bytes(s, 'utf-8')
     s2 = message
-    s3 = bytes("Message No: ", 'utf-8')
-    s4 = bytes(str(i), 'utf-8')
-    s5 = s + s2 + s3 + s4
+    #s3 = bytes("Message No: ", 'utf-8')
+    #s4 = bytes(str(i), 'utf-8')
+    #s5 = s + s2 + s3 + s4
+    s5 = s + s2
     bucket.blob('first_dist00000.txt').upload_from_string(s5, 'text/csv')
 
 #print(" "+i)
