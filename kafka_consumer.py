90d8d847c76e8e31658d5e6f485c2bb38f269f42
Soumyadev Desktop <soumyadev.poriya@gmail.com>
2023-01-16 13:25:55 +0530

    consumer slightly changed

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -12,14 +12,14 @@ bucket = client.bucket("soumyabucket")
 
 
 
-
+bytes(test_string, 'utf-8')
 i = 0
 
 for message in consumer:
     i = i + 1
     message = message.value
     s = bucket.blob('first_dist00000.txt').download_as_string()
-    s = s + message.decode() + "Message No: " + str(i)
+    s = bytes(s, 'utf-8') + message + bytes("Message No: ", 'utf-8') + bytes(str(i), 'utf-8')
     bucket.blob('first_dist00000.txt').upload_from_string(s, 'text/csv')
 
 #print(" "+i)
